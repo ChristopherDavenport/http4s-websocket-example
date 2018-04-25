f58f4331904db7ebf20f6d445db255aac1a47abf
Chris Davenport <chris@christopherdavenport.tech>
2018-04-25 16:34:55 -0400

    Add a ton more documentation to how this operates

--- src/main/scala/io/chrisdavenport/http4swebsocketexample/Server.scala
+++ src/main/scala/io/chrisdavenport/http4swebsocketexample/Server.scala
@@ -14,10 +14,39 @@ import scala.concurrent.duration._
 
 object Server {
 
-  trait Messages
-
-
-  def wsResponse[F[_]](countTo: Int)(implicit F: Effect[F], ec: ExecutionContext, S: Scheduler): F[Response[F]] = {
+  /**
+    * Generate Response Based on an Integer requested
+    * to count up to
+    * 
+    * @param countTo: Number of Seconds To Count Up To.
+    * @param F: The Effect Context To Operate Asynchronously.
+    * @param ec: ExecutionContext to operate asynchronously on.
+    * @param S: Scheduler that allows timing to be evaluated.
+    *
+    */
+  def counterResponse[F[_]](countTo: Int)(implicit F: Effect[F], ec: ExecutionContext, S: Scheduler): F[Response[F]] = {
+    
+    /**
+      * Generates the stream of websocket frames that are sent to the connected
+      * client.
+      *
+      * Messages are dequeued from the provided queue which is interrupted when
+      * the provide completed signal is flipped to true. 
+      * Asynchronously, awake every second increment the counter, then emit
+      * a message showing the count we have reached.
+      * If we have reached the final value of the counter enqueue the final message
+      * or a default message, send a close frame, and then terminate the stream.
+      * If it is not the final message at that point do nothing.
+      *
+      * @param counter: This is a counter that is incremented depending on how long
+      * the system has been operating with this request
+      * @param finalMessage: An Option of a String that will be analyzed and output
+      * when the count is complete
+      * @param completed: A Signal which when true will stop any more frames from
+      * being sent to the client
+      * @param messageQueue: This is the queue we are pulling from to generate
+      * the messages that are going out the client.
+      */
     def toClient(
       counter: async.Ref[F, Int],
       finalMessage: async.Ref[F, Option[String]],
@@ -28,12 +57,17 @@ object Server {
       .dequeue
       .interruptWhen(completed)
       .concurrently(
-          S.awakeEvery(1.seconds).evalMap{_ => counter.modify(_ + 1).flatMap{
-            case async.Ref.Change(prev, now) => 
-              messageQueue.enqueue1(Text(s"Currently $now"))
+          S.awakeEvery(1.seconds)
+          .evalMap{_ => 
+            counter.modify(_ + 1)
+            .flatMap{ 
+              case async.Ref.Change(_, now) => 
+                messageQueue.enqueue1(Text(s"Currently $now"))
                 .flatMap{_ => 
                   if (now >= countTo){
-                    finalMessage.get.flatMap{t =>  messageQueue.enqueue1(Text(t.getOrElse("No Message Received")))} *> completed.set(true)
+                    finalMessage.get.flatMap{t =>  messageQueue.enqueue1(Text(t.getOrElse("No Message Received")))} *>
+                    messageQueue.enqueue1(Close()) *>
+                    completed.set(true)
                   } else {
                     F.delay(())
                   }
@@ -42,20 +76,44 @@ object Server {
       )
     }
 
-    def fromClient(ref: async.Ref[F, Option[String]]): Sink[F, WebSocketFrame] = _.evalMap { (ws: WebSocketFrame) =>
+    /**
+      * From Client Represents What We Do With Messages Received from the Client
+      *
+      * This generally serves as a println factory for incomming frames
+      * associating when they are received. If it is a text frame the 
+      * final message returned is updated.
+      *
+      * @param counter: This is a counter that is being operated elsewhere to 
+      * inform us what is the current count when any message is received.
+      * @param ref:  This is the final message that is updated when we
+      * receive a Text frame we set that to the current final message value.
+      */
+    def fromClient(
+      counter: async.Ref[F, Int],
+      ref: async.Ref[F, Option[String]]
+    ): Sink[F, WebSocketFrame] = _.evalMap { (ws: WebSocketFrame) =>
       ws match {
-        case Text(t, _) => F.delay(println(t)) *> ref.setSync(t.some)
-        case f => F.delay(println(s"Unknown type: $f"))
+        case Text(t, _) => 
+          counter.get.flatMap{ count => 
+            F.delay(println("Count: $count " + t)) *> ref.setSync(t.some)
+          }
+        case f => 
+          counter.get.flatMap{ count => 
+            F.delay(println(s"Count: $count Unknown type: $f"))
+          }
       }
     }
 
+    // Here We Construct the Tools Required for 
+    // the toClient and fromClient Functions
+    // finally we Construct the websocket response from these.
     for {
       completed <- async.signalOf[F, Boolean](false)
       ref <- async.refOf[F, Int](0)
       finalMessage <- async.refOf[F, Option[String]](Option.empty)
       messageQueue <- async.boundedQueue[F, WebSocketFrame](100)
       clientOut = toClient(ref, finalMessage, completed, messageQueue)
-      clientIn = fromClient(finalMessage)
+      clientIn = fromClient(ref, finalMessage)
 
       resp <- WebSocketBuilder[F].build(clientOut, clientIn)
     } yield resp
@@ -69,7 +127,7 @@ object Server {
       case GET -> Root / "counter" / countTo => 
         for {
           int <- Sync[F].delay(countTo.toInt)
-          resp <- wsResponse(int)
+          resp <- counterResponse(int)
         } yield resp
     }
   }
