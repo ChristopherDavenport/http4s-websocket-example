07eab30f98faa9666c4049a8a59d65873234668d
Chris Davenport <chris@christopherdavenport.tech>
2018-04-25 13:20:50 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# http4s-websocket-example
+
---
+++ src/main/scala/io/chrisdavenport/http4swebsocketexample/Main.scala
@@ -0,0 +1,9 @@
+package io.chrisdavenport.http4swebsocketexample
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("I am a new project!")
+  }
+
+}
